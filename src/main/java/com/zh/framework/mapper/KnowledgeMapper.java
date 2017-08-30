964e90b8d5485c1d591a9e64fadd70caf3630ba5
john19961212 <1442364096@qq.com>
2017-08-30 09:23:42 +0800

    完成界面前准备

--- src/main/java/com/zh/framework/mapper/KnowledgeMapper.java
+++ src/main/java/com/zh/framework/mapper/KnowledgeMapper.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 @Repository
 public interface KnowledgeMapper {
-    public List<Knowledge> queryKnowledgesAndSort(String orderBy);
+    public List<Knowledge> queryKnowledgesAndSort(@Param("orderBy") String orderBy);
 
     public Knowledge queryKnowledgeById(String id);
 
