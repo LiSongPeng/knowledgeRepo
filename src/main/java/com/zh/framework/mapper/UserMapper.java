1abfde02627d9cc820b6581979b054cde931395a
heichen <1317708567@qq.com>
2017-09-05 11:42:50 +0800

    添加知识

--- src/main/java/com/zh/framework/mapper/UserMapper.java
+++ src/main/java/com/zh/framework/mapper/UserMapper.java
@@ -1,25 +1,25 @@
-//package com.zh.framework.mapper;
-//
-//import com.zh.framework.entity.User;
-//import com.zh.framework.sql.UserSQL;
-//import org.apache.ibatis.annotations.InsertProvider;
-//import org.apache.ibatis.annotations.ResultMap;
-//import org.apache.ibatis.annotations.SelectProvider;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-///**
-// * Created by Letg4 on 2017/9/3.
-// */
-//@Repository
-//public interface UserMapper {
-//
-//    @SelectProvider(type = UserSQL.class,method = "query")
-//    @ResultMap("com.zh.framework.mapper.UserMapper.userMap")
-//    List<User> query(Map<String,Object> param);
-//
-//    @InsertProvider(type = UserSQL.class,method = "setUserRole")
-//    int setUserRole(String uid,String rid);
-//}
+package com.zh.framework.mapper;
+
+import com.zh.framework.entity.User;
+import com.zh.framework.sql.UserSQL;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Letg4 on 2017/9/3.
+ */
+@Repository
+public interface UserMapper {
+
+    @SelectProvider(type = UserSQL.class,method = "query")
+    @ResultMap("com.zh.framework.mapper.UserMapper.userMap")
+    List<User> query(Map<String,Object> param);
+
+    @InsertProvider(type = UserSQL.class,method = "setUserRole")
+    int setUserRole(String uid,String rid);
+}
