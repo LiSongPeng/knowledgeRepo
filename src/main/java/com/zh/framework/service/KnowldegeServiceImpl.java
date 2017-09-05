9ce68b72a45ed94e15ec1da7901603ed1f72f9a2
john19961212 <1442364096@qq.com>
2017-09-05 17:34:11 +0800

    修改StringBlob类型转换器

--- src/main/java/com/zh/framework/service/KnowldegeServiceImpl.java
+++ src/main/java/com/zh/framework/service/KnowldegeServiceImpl.java
@@ -7,55 +7,54 @@ import com.zh.framework.entity.PageBean;
 import com.zh.framework.mapper.KnowledgeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service("knowledgeService")
 //public class KnowldegeServiceImpl extends BaseServiceImpl<Knowledge> implements KnowledgeService{
-public class KnowldegeServiceImpl implements KnowledgeService{
-        @Autowired
-        KnowledgeMapper knowledgeMapper;
+public class KnowldegeServiceImpl implements KnowledgeService {
+    @Autowired
+    KnowledgeMapper knowledgeMapper;
 
-        @Override
-        public PageBean queryAllKnowledge(PageBean pageBean) {
+    @Override
+    public PageBean queryAllKnowledge(PageBean pageBean) {
 
-                PageHelper.startPage(pageBean.getCurrentPage(),pageBean.getPageSize());
+        PageHelper.startPage(pageBean.getCurrentPage(), pageBean.getPageSize());
 
-                PageInfo<Knowledge> pageInfo=new PageInfo<Knowledge>(knowledgeMapper.queryAllKnowledge());
-                PageBean pb=new PageBean();
-                pb.setTotalPages(pageInfo.getPages());
-                pb.setPageSize(pageInfo.getPageSize());
-                pb.setTotalCounts((int)pageInfo.getTotal());
-                pb.setCurrentPage(pageInfo.getPageNum());
-                pb.setContent(pageInfo.getList());
-                return pb;
-        }
+        PageInfo<Knowledge> pageInfo = new PageInfo<Knowledge>(knowledgeMapper.queryAllKnowledge());
+        PageBean pb = new PageBean();
+        pb.setTotalPages(pageInfo.getPages());
+        pb.setPageSize(pageInfo.getPageSize());
+        pb.setTotalCounts((int) pageInfo.getTotal());
+        pb.setCurrentPage(pageInfo.getPageNum());
+        pb.setContent(pageInfo.getList());
+        return pb;
+    }
 
-        @Override
-        public void addKnowledge(Knowledge k) {
-                knowledgeMapper.addKnowledge(k);
+    @Override
+    public void addKnowledge(Knowledge k) {
+        knowledgeMapper.addKnowledge(k);
 
-        }
+    }
 
-        @Override
-        public void deleteKnowledge(String id) {
-                knowledgeMapper.deleteKnowledge(id);
+    @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public void deleteKnowledge(String id) {
+        knowledgeMapper.deleteKnowledge(id);
 
-        }
+    }
 
-        @Override
-        public void updateKnowledge(Knowledge k) {
+    @Override
+    public void updateKnowledge(Knowledge k) {
 
-        }
+    }
 
-        @Override
-        public void updateKnowledgeStatus(String id, String status) {
+    @Override
+    public void updateKnowledgeStatus(String id, String status) {
 
-        }
-
-
-
-
+    }
 
 
 }
