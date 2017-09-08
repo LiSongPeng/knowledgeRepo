722e5793bbfc3747eb1ba85e188ef24fa153a826
JChang07 <guaishou07@yeah.net>
2017-09-08 21:55:54 +0800

    数据库同步 Sql 文件上传

--- knowledgerepo_tb_role.sql
+++ knowledgerepo_tb_role.sql
@@ -1,2 +1,3 @@
+INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('f28165f3-3f8f-4e46-bf0d-0d53167a47d6', '赛赛', 1, 'ssss', 'bdccb13c-db1e-432c-83eb-6731dd05759d', '2017-09-08 14:47:41');
 INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('r1', 'admin', 1, 'rouro', 'sdf', '2017-09-07 11:40:16');
 INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('r2', 'asd2', 1, 'rouroururou', 'sdf', '2017-09-07 11:40:16');
