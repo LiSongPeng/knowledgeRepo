a9018234c57ff10366fb032a6ea7a114ab7eedc4
john19961212 <1442364096@qq.com>
2017-09-08 14:45:17 +0800

    添加字符过滤器

--- src/main/java/com/zh/framework/util/CharacterFilter.java
+++ src/main/java/com/zh/framework/util/CharacterFilter.java
@@ -12,6 +12,7 @@ public class CharacterFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding(encoding);
+        chain.doFilter(request,response);
     }
 
     @Override
