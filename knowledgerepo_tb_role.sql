935644ed38c5e3c56dcf6de303ce36e9388f74a3
JChang07 <guaishou07@yeah.net>
2017-09-11 09:53:33 +0800

    各种修改 。权限验证

--- knowledgerepo_tb_role.sql
+++ knowledgerepo_tb_role.sql
@@ -1,3 +1,3 @@
-INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('f28165f3-3f8f-4e46-bf0d-0d53167a47d6', '赛赛', 1, 'ssss', 'bdccb13c-db1e-432c-83eb-6731dd05759d', '2017-09-08 14:47:41');
-INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('r1', 'admin', 1, 'rouro', 'sdf', '2017-09-07 11:40:16');
+INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('1ee05a23-af17-45b2-a6e6-b1ee9115122b', '测试角色', 1, '测试', 'bdccb13c-db1e-432c-83eb-6731dd05759d', '2017-09-11 03:05:02');
+INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('r1', 'admin', 1, 'rouro', 'bdccb13c-db1e-432c-83eb-6731dd05759d', '2017-09-07 11:40:16');
 INSERT INTO knowledgerepo.tb_role (id, rName, deleteStatus, rDescription, createUserId, createTime) VALUES ('r2', 'asd2', 1, 'rouroururou', 'sdf', '2017-09-07 11:40:16');
--- knowledgerepo_tb_role_user.sql
+++ knowledgerepo_tb_role_user.sql
@@ -3,3 +3,4 @@ INSERT INTO knowledgerepo.tb_role_user (rId, uId) VALUES ('r1', 'e64db4d0-d85d-4
 INSERT INTO knowledgerepo.tb_role_user (rId, uId) VALUES ('r2', 'e64db4d0-d85d-4020-b390-942bf0a8a45b');
 INSERT INTO knowledgerepo.tb_role_user (rId, uId) VALUES ('f28165f3-3f8f-4e46-bf0d-0d53167a47d6', 'bdccb13c-db1e-432c-83eb-6731dd05759d');
 INSERT INTO knowledgerepo.tb_role_user (rId, uId) VALUES ('r1', 'bdccb13c-db1e-432c-83eb-6731dd05759d');
+INSERT INTO knowledgerepo.tb_role_user (rId, uId) VALUES ('1ee05a23-af17-45b2-a6e6-b1ee9115122b', 'e5af514d-7342-42ed-8081-cec6c85c5564');
